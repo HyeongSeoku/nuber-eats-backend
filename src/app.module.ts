9db0e467c001858b4dd145ffe55d8bf7cd27c36b
HyeongSeoku <gudtjr3437@naver.com>
2021-12-06 16:24:06 +0900

    Feat : 모듈 추가 & autoSchema 추가

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { RestaurantsModule } from './restaurants/restaurants.module';
+
+@Module({
+  imports: [
+    //GraphQL을 루트로 설정
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+    RestaurantsModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
