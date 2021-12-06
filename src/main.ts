b888e8c1f8f20a344cc48825605e0d3448f989ea
HyeongSeoku <gudtjr3437@naver.com>
2021-12-06 16:25:14 +0900

    Feat : main.ts 생성

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  //NestFactory가 AppModule로부터 application 생성
+  //AppModuled에다 GraphQL 모듈을 추가하여야 함
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+bootstrap();
+
+// application 실행하기 위한 파일
