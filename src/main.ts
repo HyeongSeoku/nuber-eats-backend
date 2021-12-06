1e28266cd454432b350b6c30a3d07c844c3a7f74
HyeongSeoku <gudtjr3437@naver.com>
2021-12-06 17:39:20 +0900

    Feat : validator 추가

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -5,6 +6,7 @@ async function bootstrap() {
   //NestFactory가 AppModule로부터 application 생성
   //AppModuled에다 GraphQL 모듈을 추가하여야 함
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
--- src/restaurants/dtos/create-restauarnt.dto.ts
+++ src/restaurants/dtos/create-restauarnt.dto.ts
@@ -1,4 +1,5 @@
 import { ArgsType, Field, InputType } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
 
 //InputType = 하나의 Object argument로써 graphql에 전달하기 위한 용도
 //ArgsType =  분리된 값들을 GraphQL argument로 전달할 수 있도록 함
@@ -6,14 +7,19 @@ import { ArgsType, Field, InputType } from '@nestjs/graphql';
 @ArgsType()
 export class CreateRestaurantDto {
   @Field(() => String)
+  @IsString() //유효성 검사 부분
+  @Length(5, 10) //길이 유효성 (최소,최대)  (validation-pipeline생성 안하면 작동 X)
   name: string;
 
   @Field(() => Boolean)
+  @IsBoolean()
   isVegan: boolean;
 
   @Field(() => String)
+  @IsString()
   address: string;
 
   @Field(() => String)
+  @IsString()
   ownerName: string;
 }
