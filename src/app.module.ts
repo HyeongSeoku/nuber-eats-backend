fa7055c28e9d7a0a10a939bf0e02270183cda713
HyeongSeoku <gudtjr3437@naver.com>
2021-12-07 17:04:32 +0900

    Feat : PostgreSQL에 테이블 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as Joi from 'joi';
+import { Restaurant } from './restaurants/entities/restaurant.entity';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+
+console.log(Joi);
 
 @Module({
   imports: [
@@ -10,6 +14,15 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
       ignoreEnvFile: process.env.NODE_EV === 'prod',
+      //필요한 파일들이 모두 있지 않으면 실행되지 않도록 하기 위한 joi
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
     }),
     TypeOrmModule.forRoot({
       //코드에 직접 쓰거나 ormconfig.json이라는 파일에 작성
@@ -19,8 +32,9 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWD, //localhost 일경우 password필요없음
       database: process.env.DB_NAME,
-      synchronize: true,
+      synchronize: process.env.NODE_EV !== 'prod', //production에서는 실제 데이터를 가지고 있기 때문
       logging: true,
+      entities: [Restaurant],
     }),
     //GraphQL을 루트로 설정
     GraphQLModule.forRoot({
--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,16 +1,26 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryColumn } from 'typeorm';
 
-@ObjectType()
+@ObjectType() //graphQL을 위한 ObjectType
+@Entity() //TypeORM을 위한 Entity
 export class Restaurant {
-  @Field(() => String)
+  @Field((type) => Int)
+  @PrimaryColumn() // TypeORM PrimayColumn을 넣어줘야 오류 안뜸
+  id: number;
+
+  @Field(() => String) //GraphQL
+  @Column() //TypeORM
   name: string;
 
   @Field(() => Boolean)
+  @Column()
   isVegan?: boolean;
 
   @Field(() => String)
+  @Column()
   address?: string;
 
   @Field(() => String)
+  @Column()
   ownerName?: string;
 }
