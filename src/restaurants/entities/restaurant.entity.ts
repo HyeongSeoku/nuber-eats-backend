29f5b169024848e1f88f0f46a0c6653dd02b0cd9
HyeongSeoku <gudtjr3437@naver.com>
2021-12-09 23:56:08 +0900

    Feat : Activity Record 사용시 필요한 것 주석 작성

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,6 +1,9 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql';
 import { Column, Entity, PrimaryColumn } from 'typeorm';
 
+//TypeORM에서 Active Record 사용 원하면
+//export class Restaurant extends BaseEntity {} 이런 식으로 작성해야함
+
 @ObjectType() //graphQL을 위한 ObjectType
 @Entity() //TypeORM을 위한 Entity
 export class Restaurant {
