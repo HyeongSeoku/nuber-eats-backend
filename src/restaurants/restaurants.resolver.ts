2d1ddc004d0c881fc235d06a0e9d09869fb9995c
HyeongSeoku <gudtjr3437@naver.com>
2021-12-10 01:36:42 +0900

    Feat : service의 getAll메소드 실행하도록 변경

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -2,21 +2,22 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { BooleanValueNode } from 'graphql';
 import { CreateRestaurantDto } from './dtos/create-restauarnt.dto';
 import { Restaurant } from './entities/restaurant.entity';
+import { RestaurantService } from './restaurants.service';
 //Query nestjs/graphql 인지 꼭 확인
 
 @Resolver((of) => Restaurant) //(of) => Restaurant : 필수는 아님
 export class RestaurantsResolver {
+  //RestaurantService import
+  constructor(private readonly restaurantService: RestaurantService) {}
   @Query((returns) => [Restaurant]) //GraphQL의 방식 : [Restaurant]
-  restaurant(@Args('veganOnly') veganOnly: string): Restaurant[] {
-    console.log(typeof veganOnly);
-    return [];
+  restaurant(): Promise<Restaurant[]> {
+    return this.restaurantService.getAll();
   }
 
   @Mutation((returns) => Boolean)
   createRestaurant(
     @Args() createRestaurantInput: CreateRestaurantDto,
   ): boolean {
-    console.log(createRestaurantInput);
     return true;
   }
 }
