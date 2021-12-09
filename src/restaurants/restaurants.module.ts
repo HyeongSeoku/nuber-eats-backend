54e26ec0306c17a00cf6be59a8c2196aa2e1a964
HyeongSeoku <gudtjr3437@naver.com>
2021-12-10 01:34:17 +0900

    Feat : 엔티티 import & Service 추가

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Restaurant } from './entities/restaurant.entity';
 import { RestaurantsResolver } from './restaurants.resolver';
+import { RestaurantService } from './restaurants.service';
 
 @Module({
-  //resolver 필요
-  providers: [RestaurantsResolver],
+  //entity가 여러개면 forFeature([one,two,three]) 여러개 작성
+  imports: [TypeOrmModule.forFeature([Restaurant])], //엔티티 import
+  //resolver,Service 필요
+  providers: [RestaurantsResolver, RestaurantService],
 })
 export class RestaurantsModule {}
