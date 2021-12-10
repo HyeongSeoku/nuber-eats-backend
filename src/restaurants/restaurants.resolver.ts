8077aff3f141d450e9feb9c4e47accd8215149dd
HyeongSeoku <gudtjr3437@naver.com>
2021-12-10 15:35:40 +0900

    Feat : 레스토랑 생성 메소드 추가

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -15,9 +15,15 @@ export class RestaurantsResolver {
   }
 
   @Mutation((returns) => Boolean)
-  createRestaurant(
-    @Args() createRestaurantInput: CreateRestaurantDto,
-  ): boolean {
-    return true;
+  async createRestaurant(
+    @Args() createRestaurantDto: CreateRestaurantDto,
+  ): Promise<boolean> {
+    try {
+      await this.restaurantService.createRestaurant(createRestaurantDto); //async 와 Promise필요
+      return true;
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
   }
 }
--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateRestaurantDto } from './dtos/create-restauarnt.dto';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Injectable()
@@ -13,5 +14,11 @@ export class RestaurantService {
   //모든 레스토랑을 가져오는 service (실제로 DB에 접근하는 방식)
   getAll(): Promise<Restaurant[]> {
     return this.restaurants.find(); //find()는 async method
+  }
+  createRestaurant(
+    createRestaurantDto: CreateRestaurantDto,
+  ): Promise<Restaurant> {
+    const newRestaurant = this.restaurants.create(createRestaurantDto); //생성. js에만 생성돼있고 DB상 저장 안되어있음
+    return this.restaurants.save(newRestaurant); //DB에 저장 (Promise 필요)
   }
 }
